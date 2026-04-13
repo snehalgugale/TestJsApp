a9bc46724c2098675e1234280cc501d25faa268a
snehal gugale <snehal.gugale@kanakasoftware.com>
2026-04-13 11:39:04 +0530

    First commit

---
+++ src/Users/users.js
@@ -0,0 +1,6 @@
+
+
+// Define a route for the home page ('/')
+export default function(req, res) {
+res.send('Hello World! This is a simple Express app.');
+} 
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+const PORT = 3000;
+import user from "./Users/users.js"
+
+app.get('/', (req, res) => {
+  user(req, res);
+})
+// Start the server and listen for incoming requests on the specified port
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
